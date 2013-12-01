94c371b6ddbf16dfced932fcd0a393a16154fbf3
yozec <mw280448@students.mimuw.edu.pl>
2013-12-01 16:36:17 +0100

    Update pfamparser.py
    Parser for FASTA proteins file and PFAM domains file from http://pfam.sanger.ac.uk/

--- Bio/pfamparser.py
+++ Bio/pfamparser.py
@@ -4,36 +4,84 @@ import sys
 from Bio.Seq import Seq
 from Bio.SeqRecord import SeqRecord
 
-if len(sys.argv)<3:
-	print "\n\t**Pfamparser takes 2 arguments: pfamseq.move i swisspfam**\n\t**You can download it from ftp://ftp.sanger.ac.uk/pub/databases/Pfam/releases/ **\n\t**After You choosen Pfam version, you sholud download pfamseq.gz and swisspfam.gz **\n"
-	exit(1)
 
-f_seq = open(sys.argv[1])
-lines = f_seq.readlines()
-f_seq.close()
+'''
+Pfamparser needs 2 file: pfamseq.move (FASTA proteins file) and swisspfam (file with protein domains).
+You can download this files from ftp://ftp.sanger.ac.uk/pub/databases/Pfam/releases/.
 
-proteins_hash={}
+Script has 4 methods:
+
+FASTAparser takes file pfamseq.move and return a dictionary: {proteinId:[proteinObject]}. The value is a list.
+
+PFAMparser takes file swisspfam and dictionary with proteins from FASTAparse, 
+create domain object, which sequence is from slicing protein sequence and add it to the proteinObject list. 
+Proteins and domains are SeqReceord objects. It returns a dictionary: {proteinId:[proteinObject,[domainObject,...,domainObject].
+The coordinates domains in protein sequence are in domain.feature. 
+
+Methods proteinsViewer view single protein record and domainsViewer view all of the protein domains.
 
 
-for i in xrange(len(lines)):
+Exemple:
+
+If you want to print record of ACON_BRAJA protein:
+
+###print proteins_viewer(proteins_dict['ACON_BRAJA'])
+
+and you should get:
+
+ID: ACON_BRAJA
+Name: <unknown name>
+Description: 920 ACONITATE HYDRATASE (EC 4.2.1.3) (CITRATE HYDRO-LYASE) (ACONITASE).
+Number of features: 0
+'MTSLDSFKCKKTLKVGAKTYVYYSLPTAEK[...]ICQSFERIHRSNLVGMGVLPLTFEEGTSWSSLGLKGDEKVTLRGLVGDLKPRQKLTAEIVSGDGSLQRVSLLCRIDTLDELDYYRNGGILHYVLRKLAA'
+
+###print domains_viewer(proteins_dict['120K_RICRI'])
+
+
+ID: Pfam-B_112
+Name: <unknown name>
+Description: <unknown description>
+Number of features: 2
+'IVNATTLYAGISTLNNNQGTVTLSGGVPNTP[...]TSIETTLTLANGNIGHIVILEGAQVNTTTTGTTTIKVQDNANANFSGTQTYT'
+
+ID: Pfam-B_112
+Name: <unknown name>
+Description: <unknown description>
+Number of features: 2
+'GTVINGKVNQTALVGGALAAGTITLDGSAT[...]DANVGSFVFNAGGTNIVSGTVGGQQGNKFNTVALENGTTVKFLGNATFNGNTTIAANSTL'
+
+ID: Pfam-B_19744
+Name: <unknown name>
+Description: <unknown description>
+Number of features: 2
+'MVIQSANATGQVNFRHIVDVGADGTTAFK[...]VAVTNNITAIEASGAGVVQLSGTHAAELRLGNAGSIFKLAD'
+
+'''
+
+def FASTAparser(pfamseq):
+
+	proteinsDict={}
+
+	for i in xrange(len(pfamseq)):
 	
-	line=lines[i].split()
+		line=pfamseq[i].split()
 	
-	if line[0][0]=='>':
+		if line[0][0]=='>':
+			proteinId=line[0][1:]
+			proteinAnnotation=pfamseq[i][15:].rstrip()
+			sequence=''
 		
-		sequence=''
-		
-		for j in xrange(i+1, len(lines)):
-			if lines[j][0]!='>':
-				sequence=sequence+lines[j].rstrip()
-			else:
-
-				protein=SeqRecord(sequence)
-				protein.id=line[0][1:]
-				protein.description=lines[i][15:].rstrip()
-				proteins_hash[protein.id]=protein
-				break
-
+			for j in xrange(i+1, len(pfamseq)):
+				if pfamseq[j][0]!='>':
+					sequence=sequence+pfamseq[j].rstrip()
+				else:
+	
+					protein=SeqRecord(sequence)
+					protein.id=proteinId
+					protein.description=proteinAnnotation
+					proteinsDict[proteinId]=[protein]
+					break
+	return proteinsDict
 	
 
 f = open(sys.argv[2])
@@ -41,44 +89,67 @@ f = open(sys.argv[2])
 lines = f.readlines()
 f.close()
 
+def PFAMparser(swisspfam, protDict):
 
-for i in xrange(0, len(lines)):
+	for i in xrange(0, len(swisspfam)):
 
-	line=[]
+		line=[]
 	
-	line=lines[i].split()
+		line=swisspfam[i].split()
 	
-	if len(line)>0:
+		if len(line)>0:
 		
-		if line[0][0]=='>' and line[0][1:] in proteins_hash:
-			protein_id=line[0][1:]
-			coordinate=[]
+			if line[0][0]=='>' and line[0][1:] in protDict:
+				proteinId=line[0][1:]
+				thisProteinSequence=protDict[proteinId][0].seq
+				coordinate=[]
+				domainsList=[]
 
-			for j in xrange(i+1, len(lines)):
+				for j in xrange(i+1, len(swisspfam)):
 							
-				if lines[j]!='\n':
+					if swisspfam[j]!='\n':
 
-					line_domain=lines[j].split()
+						domainLine=swisspfam[j].split()
 
-					ile_domain=int(line_domain[1])
+						numberOfDomains=int(domainLine[1])
 					
-					for y in xrange(ile_domain):		
-						coordinate_one_domain=line_domain[-1-y].split('-')
+						for y in xrange(numberOfDomains):		
+							coordinatesOfSingleDomain=domainLine[-1-y].split('-')
 						
-						for z in xrange(2):
-							coordinate_one_domain[z]=int(coordinate_one_domain[z])
-							coordinate.append(coordinate_one_domain[z])
+							for z in xrange(2):
+								coordinatesOfSingleDomain[z]=int(coordinatesOfSingleDomain[z])
+								coordinate.append(coordinatesOfSingleDomain[z])
 
-						domain=SeqRecord(proteins_hash[protein_id].seq[coordinate_one_domain[0]-1:coordinate_one_domain[1]])
-						domain.id=line_domain[0]
-						domain.features=coordinate_one_domain
-						proteins_hash[protein_id].annotations[domain.seq]=domain
+							domain=SeqRecord(thisProteinSequence[coordinatesOfSingleDomain[0]-1:coordinatesOfSingleDomain[1]])
+							domain.id=domainLine[0]
+							domain.features=coordinatesOfSingleDomain
+							domainsList.append(domain)
 
-				else:
-					i=j+1
-					break
+					else:
+						protDict[proteinId].append(domainsList)
+						i=j+1
+						break
+	return protDict
+
+
+def domainsViewer(domains):
+	for i in domains[1]:
+		print "\n",i
+
+def proteinsViewer(protein):
+	print protein[0]
 
 
 
-#print proteins_hash['P74763'].protein_sequence
-#print proteins_hash['11S3_HELAN'].annotation['MASKATLLLAFTLLFATCIARHQQRQQQQNQCQLQNIEALEPIEVIQAEAGVTEIWDAYDQQFQCAWSILFDTGFNLVAFSCLPTSTPLFWPSSREGVILPGCRRTYEYSQEQQFSGEGGRRGGGEGTFRTVIRKLENLKEGDVVAIPTGTAHWLHNDGNTELVVVFLDTQNHENQLDENQRRFFLAGNPQAQAQSQQQQQRQPRQQSPQRQRQRQRQGQGQNAGNIFNGFTPELIAQSFNVDQETAQKLQGQNDQRGHIVNVGQDLQIVRPPQDRRSPRQQQEQATSPRQQQEQQQGRRGGWSNGVEETICSMKFKVNIDNPSQADFVNPQAGSIANLNSFKFPILEHLRLSVERGELRPNAIQSPHWTINAHNLLYVTEGALRVQIVDNQGNSVFDNELREGQVVVIPQNFAVIKRANEQGSRWVSFKTNDNAMIANLAGRVSASAASPLTLWANRYQLSREEAQQLKFS'].domain.id
+#This is default settings - files pfamparser.py, pfamseq.move and swisspfam should be in the same direcetion.
+fSeq = open('pfamseq.move')
+proteins = fSeq.readlines()
+fSeq.close()
+
+
+fSeq = open('swisspfam')
+domains = fSeq.readlines()
+fSeq.close()
+proteinsWithDomains=PFAMparser(domains, FASTAparser(proteins))
+
+
